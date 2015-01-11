51f4a3e8161eb18bc9c8dd9aeaa8289331ffdd0e
Jessica Meyer <JessLynnMeyer@gmail.com>
2015-01-11 14:56:56 -0600

    update server.rb file temporarily to test

--- server.rb
+++ server.rb
@@ -90,35 +90,48 @@ end
 # END OPTION: SPLIT SIGNUP/IN 
 ####################
 
-
-get "/welcome/:username" do
-  erb :"welcome"
-end 
-
-post "/welcome" do
-  # redirect to "/story/" + params['hashtag'] + "?title=" + params['title'].downcase.gsub(' ', '-')
-  redirect to "/story/" + params['hashtag'] +"?start-date="+ params["date-of-start"] + "&end-date="+ params["date-of-end"] 
+get "/welcome" do 
+  erb :welcome
 end
 
-get "/story/:x" do
-  starttime = params["start-date"].split("-")
-  y = starttime[0].to_i
-  m = starttime[1].to_i
-  d = starttime[2].to_i
-  @startdate = Date.new(y,m,d).to_time.to_i 
-  endtime = params["end-date"].split("-")
-  ey = endtime[0].to_i
-  em = endtime[1].to_i
-  ed = endtime[2].to_i
-  @enddate = Date.new(ey,em,ed).to_time.to_i
-  puts params  
-  @title = params['title']
+# get "/welcome/:username" do
+#   erb :"welcome"
+# end 
 
-  @data = JSON.parse RestClient.get 'https://api.instagram.com/v1/tags/'+ params['x']+ '/media/recent?access_token=1523996703.e61ce71.055273204cd2431c843615792dc40304&max_tag_id=@enddate&min_tag_id=@startdate&count=6'
-  # @data = JSON.parse RestClient.get 'https://api.instagram.com/v1/tags/'+ params['x']+ '/media/recent?access_token=1523996703.e61ce71.055273204cd2431c843615792dc40304'
-
-  erb :"story"
+post "/welcome" do 
+  redirect to "/story/" + params['hashtag']
 end
+
+# post "/welcome" do
+#   # redirect to "/story/" + params['hashtag'] + "?title=" + params['title'].downcase.gsub(' ', '-')
+#   redirect to "/story/" + params['hashtag'] +"?start-date="+ params["date-of-start"] + "&end-date="+ params["date-of-end"] 
+# end
+
+get "/story/:x" do 
+  puts params
+  @data = JSON.parse RestClient.get 'https://api.instagram.com/v1/tags/'+ params['x']+ '/media/recent?access_token=1523996703.e61ce71.055273204cd2431c843615792dc40304'
+  erb :story
+end
+
+# get "/story/:x" do
+#   starttime = params["start-date"].split("-")
+#   y = starttime[0].to_i
+#   m = starttime[1].to_i
+#   d = starttime[2].to_i
+#   @startdate = Date.new(y,m,d).to_time.to_i 
+#   endtime = params["end-date"].split("-")
+#   ey = endtime[0].to_i
+#   em = endtime[1].to_i
+#   ed = endtime[2].to_i
+#   @enddate = Date.new(ey,em,ed).to_time.to_i
+#   puts params  
+#   @title = params['title']
+
+#   @data = JSON.parse RestClient.get 'https://api.instagram.com/v1/tags/'+ params['x']+ '/media/recent?access_token=1523996703.e61ce71.055273204cd2431c843615792dc40304&max_tag_id=@enddate&min_tag_id=@startdate&count=6'
+#   # @data = JSON.parse RestClient.get 'https://api.instagram.com/v1/tags/'+ params['x']+ '/media/recent?access_token=1523996703.e61ce71.055273204cd2431c843615792dc40304'
+
+#   erb :"story"
+# end
 
 
 
